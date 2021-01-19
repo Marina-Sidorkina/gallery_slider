6cefd6efff668dd7420f90c889735db60cb7149a
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-01-20 01:22:18 +0300

    Add photos switching.

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 const track = document.querySelector('.gallery_track');
-track.style.transform = "translateX(-100px)";
+const buttonLeft = document.querySelector('.gallery_button__left');
+const buttonRight = document.querySelector('.gallery_button__right');
+const photosAmount = document.querySelectorAll('.gallery_item').length;
+const shiftStep = document.querySelector('.gallery_item').offsetWidth;
+let count = 1;
+let shift = 0;
+
+function moveRigth() {
+  if(count !== photosAmount) {
+    shift -= shiftStep;
+    track.style.transform = `translateX(${shift}px)`;
+    count++;
+  }
+}
+
+function moveLeft() {
+  if(count !== 1) {
+    shift += shiftStep;
+    track.style.transform = `translateX(${shift}px)`;
+    count--;
+  }
+}
+
+buttonRight.addEventListener('click', () => {
+  moveRigth()
+});
+
+buttonLeft.addEventListener('click', () => {
+  moveLeft()
+});
+
+// Touch Slider on native JavaScript https://codepen.io/i-did/pen/yLYRvVL
+// Hammer.js https://hammerjs.github.io/getting-started/
+// Bootstrap carousel https://getbootstrap.com/docs/4.0/components/carousel/
+// Example https://itchief.ru/examples/lab.php?topic=javascript&file=chiefslider-example-12
